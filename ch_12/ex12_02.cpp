459c50adcdd4dd39534d05eb741079f877fc8454
n1-k0 <43737798+n1-k0@users.noreply.github.com>
2018-10-13 21:47:28 +0800

    Create ex12_02.cpp

---
+++ ch_12/ex12_02.cpp
@@ -0,0 +1,54 @@
+#pragma once
+#ifndef EX12_02
+#define EX12_02
+
+#include <vector>
+#include <string>
+#include <memory>
+#include <initializer_list>
+#include <exception>
+
+class StrBlob {
+public:
+	typedef std::vector<std::string>::size_type size_type;
+	StrBlob(): data( std::make_shared<std::vector<std::string>>() ) {};
+	StrBlob(std::initializer_list<std::string> il);
+	size_type size() const { return data->size(); }
+	bool empty() const { return data->empty(); }
+	void push_back(const std::string &t) { data->push_back(t); }
+	void pop_back() {
+		check(0, "pop_back on empty StrBlob");
+		data->pop_back();
+	}
+
+	std::string& front() {
+		check(0, "front on empty StrBlob");
+		return data->front();
+	}
+	std::string& back() {
+		check(0, "back on empty StrBlob");
+		return data->back();
+	}
+
+	const std::string& front() const {
+		check(0, "front on empty StrBlob");
+		return data->front();
+	}
+	const std::string& back() const {
+		check(0, "back on empty StrBlob");
+		return data->back();
+	}
+
+private:
+	std::shared_ptr<std::vector<std::string>> data;
+	void check(size_type i, const std::string &msg) const {
+		if (i >= data->size())
+			throw std::out_of_range(msg);
+	}
+};
+
+
+
+
+#endif // !EX12_02
+
