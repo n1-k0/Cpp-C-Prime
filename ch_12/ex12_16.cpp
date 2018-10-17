bc98c8a9ccd0c21d747610c940a4b6375d7c9489
n1-k0 <43737798+n1-k0@users.noreply.github.com>
2018-10-17 16:40:50 +0800

    Update ex12_16.cpp

--- ch_12/ex12_16.cpp
+++ ch_12/ex12_16.cpp
@@ -7,16 +7,20 @@ using namespace std;
 int main()
 {
 	unique_ptr<int> up(new int(12));
-	unique_ptr<int> up2 = up;
+	//unique_ptr<int> up2 = up;
+	/*
+		严重性	代码	说明	项目	文件	行	禁止显示状态
+		错误(活动)	E1776	无法引用 函数 "std::unique_ptr<_Ty, _Dx>::unique_ptr(const std::unique_ptr<_Ty, _Dx> &)
+		[其中 _Ty=int, _Dx=std::default_delete<int>]"
+		-- 它是已删除的函数
+	*/
+	unique_ptr<int> up3(up);
 	/*
 		严重性	代码	说明	项目	文件	行	禁止显示状态
 		错误(活动)	E1776	无法引用 函数 "std::unique_ptr<_Ty, _Dx>::unique_ptr(const std::unique_ptr<_Ty, _Dx> &) 
 		[其中 _Ty=int, _Dx=std::default_delete<int>]" 
-	*/
-	unique<int> up3(up);
-	/*
-		严重性	代码	说明	项目	文件	行	禁止显示状态
-		错误(活动)	E1776	无法引用 函数 "std::unique_ptr<_Ty, _Dx>::unique_ptr(const std::unique_ptr<_Ty, _Dx> &) 
+		-- 它是已删除的函数	
+
 	*/
 
 	return 0;
