62652ddc566e2be9beb38ac68224184dad0734fa
n1-k0 <43737798+n1-k0@users.noreply.github.com>
2018-10-17 16:39:14 +0800

    Create ex12_16.cpp

---
+++ ch_12/ex12_16.cpp
@@ -0,0 +1,23 @@
+#include "pch.h"
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+int main()
+{
+	unique_ptr<int> up(new int(12));
+	unique_ptr<int> up2 = up;
+	/*
+		严重性	代码	说明	项目	文件	行	禁止显示状态
+		错误(活动)	E1776	无法引用 函数 "std::unique_ptr<_Ty, _Dx>::unique_ptr(const std::unique_ptr<_Ty, _Dx> &) 
+		[其中 _Ty=int, _Dx=std::default_delete<int>]" 
+	*/
+	unique<int> up3(up);
+	/*
+		严重性	代码	说明	项目	文件	行	禁止显示状态
+		错误(活动)	E1776	无法引用 函数 "std::unique_ptr<_Ty, _Dx>::unique_ptr(const std::unique_ptr<_Ty, _Dx> &) 
+	*/
+
+	return 0;
+}
