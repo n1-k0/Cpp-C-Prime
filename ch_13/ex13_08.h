494cca4c07608208e373df32083b8a9dfbea207f
n1-k0 <43737798+n1-k0@users.noreply.github.com>
2018-11-07 15:51:00 +0800

    Add files via upload

---
+++ ch_13/ex13_08.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef EX_13_08_H
+#define EX_13_08_H
+#include <string>
+
+class HasPtr {
+public:
+	HasPtr(const std::string &s = std::string()) :
+		ps(new std::string(s)), i(0) {}
+	HasPtr(const HasPtr& hp) : ps(new std::string(*hp.ps)), i(hp.i) {}
+	HasPtr& operator = (const HasPtr& hp) {
+		if (this != &hp) {
+			auto newp = new string(*hp.ps);
+			delete ps;
+			ps = newp;
+			i = hp.i;
+		}
+	}
+private:
+	int i;
+	std::string *ps;
+};
+
+#endif
